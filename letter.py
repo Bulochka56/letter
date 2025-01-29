4e60d77250c34a23183b98683665a6a5110ca73e
Bulochka56 <bul.xajrullin@yandex.ru>
2025-01-29 20:44:39 +0300

    Update 0.25

--- letter.py
+++ letter.py
@@ -3,10 +3,8 @@ import torch
 import requests
 import streamlit as st
 from PIL import Image
-from transformers import VisionEncoderDecoderModel,TrOCRProcessor
+
 from transformers import pipeline
-
-
 
 #кэшируем модель
 @st.cache_data
@@ -14,14 +12,6 @@ def load_model():
     return pipeline("image-to-text", model="kazars24/trocr-base-handwritten-ru")
 #def load_processor():
     #return TrOCRProcessor.from_pretrained("kazars24/trocr-base-handwritten-ru")
-
-# Функция распознавания объектов на изображении
-#def predict_step(image):
-  #image = image.convert("RGB")
-  #pixel_values = processor(images=image, return_tensors="pt").pixel_values
-  #generated_ids = model.generate(pixel_values)
-  #generated_text = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
-  #return generated_text
 
 # Функция загрузки ихображения через Streamlit
 def load_image():
@@ -33,9 +23,7 @@ def load_image():
     else:
         return None
 
-st.title('Распознавание письменного текста с картинки')
-#model = load_model()	
-#processor = load_processor()    
+st.title('Распознавание письменного текста с изображения')    
 image_to_text = load_model()
 
 im=load_image()
