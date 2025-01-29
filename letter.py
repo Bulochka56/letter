a27784ee4ffa02bce1a359b5e6b0da7a995dbec6
Bulochka56 <bul.xajrullin@yandex.ru>
2025-01-29 15:52:55 +0300

    Update 0.2

--- letter.py
+++ letter.py
@@ -4,21 +4,24 @@ import requests
 import streamlit as st
 from PIL import Image
 from transformers import VisionEncoderDecoderModel,TrOCRProcessor
+from transformers import pipeline
+
+
 
 #кэшируем модель
 @st.cache_data
-def load_model():
-    return VisionEncoderDecoderModel.from_pretrained("kazars24/trocr-base-handwritten-ru")
-def load_processor():
-    return TrOCRProcessor.from_pretrained("kazars24/trocr-base-handwritten-ru")
+#def load_model():
+    #return VisionEncoderDecoderModel.from_pretrained("kazars24/trocr-base-handwritten-ru")
+#def load_processor():
+    #return TrOCRProcessor.from_pretrained("kazars24/trocr-base-handwritten-ru")
 
 # Функция распознавания объектов на изображении
-def predict_step(image):
-  image = image.convert("RGB")
-  pixel_values = processor(images=image, return_tensors="pt").pixel_values
-  generated_ids = model.generate(pixel_values)
-  generated_text = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
-  return generated_text
+#def predict_step(image):
+  #image = image.convert("RGB")
+  #pixel_values = processor(images=image, return_tensors="pt").pixel_values
+  #generated_ids = model.generate(pixel_values)
+  #generated_text = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
+  #return generated_text
 
 # Функция загрузки ихображения через Streamlit
 def load_image():
@@ -31,13 +34,14 @@ def load_image():
         return None
 
 st.title('Распознавание письменного текста с картинки')
-model = load_model()	
-processor = load_processor()    
-            
+#model = load_model()	
+#processor = load_processor()    
+image_to_text = pipeline("image-to-text", model="kazars24/trocr-base-handwritten-ru")
+
 im=load_image()
 sleep_duration = 0.5
 result = st.button('Распознать:')
 if result:
-   preds = predict_step(im)
+   preds = image_to_text(im)
    st.write('**На картинке:**')
    st.write(str(preds))
